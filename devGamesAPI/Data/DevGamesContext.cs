685389f675f8fcc0ce759b390648b6c46e6ee7e9
May <senaidev.may@gmail.com>
2025-02-05 11:53:43 -0300

    Adicionar arquivos de projeto.

---
+++ devGamesAPI/Data/DevGamesContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace devGamesAPI.Data
+{
+    public class DevGamesContext : DbContext
+    {
+    }
+}
---
+++ devGamesAPI/Program.cs
@@ -0,0 +1,94 @@
+using devGamesAPI.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Configurar a conexão com o banco de dados
+builder.Services.AddDbContext<DevGamesContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Configurar o CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+// Adionar o Swagger com JWT Bearer
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+    {
+        new OpenApiSecurityScheme
+        {
+        Reference = new OpenApiReference
+            {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+            },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+
+        },
+        new List<string>()
+        }
+    });
+});
+
+// Serviço de EndPoints do Identity Framework
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 4;
+
+})
+    .AddEntityFrameworkStores<DevGamesContext>()
+    .AddDefaultTokenProviders(); //Adicionando o provedor de tokens padrão
+
+//Adicionar Serviço de Autenticação e Autorização
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+//Swagger em ambiente de desenvolvimento
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//Mapear os EndPoints padrões do Identity Framework
+app.MapGroup("/Users").MapIdentityApi<IdentityUser>();
+app.MapGroup("/Roles").MapIdentityApi<IdentityRole>();
+
+app.UseHttpsRedirection();
+
+//Permitir a Autenticação e Autorização de qualquer origem
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseCors("AllowAll");
+
+app.MapControllers();
+
+app.Run();
